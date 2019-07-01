00f224cea76a9d629be73ba93309fef21a4dc72d
XiangWang <muumuu123@126.com>
2019-07-01 23:00:53 +0800

    add

---
+++ w3cWeb/tornado/studyDemo/Senior/01_cookie.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   01_cookie.py
+@Time    :   2019/07/01 21:30:22
+@Author  :   王翔 
+@Version :   1.0
+@Contact :   muumuu123@126.com
+@License :   (C)Copyright 2018-2019, 风蓝科技
+@Desc    :   None
+'''
+
+# here put the import lib
+import tornado.web
+import tornado.ioloop
+import time
+
+class IndexHandler(tornado.web.RequestHandler):
+    # 主页处理类
+    def get(self):
+        #写cookie
+        # 原理 设置cookie实际就是通过设置header的Set-Cookie来实现的。
+        self.set_cookie("n1", "v1")
+        self.set_cookie("n2", "v2", path="/new", expires=time.strptime("2016-11-11 23:59:59","%Y-%m-%d %H:%M:%S"))
+        self.set_cookie("n3", "v3", expires_days=20)
+        # 利用time.mktime将本地时间转换为UTC标准时间
+        self.set_cookie("n4", "v4", expires=time.mktime(time.strptime("2016-11-11 23:59:59","%Y-%m-%d %H:%M:%S")))
+        
+        #读取cookie
+        n3 = self.get_cookie("n3")
+        self.write(str(n3))
+
+        #清除cookie
+        # clear_cookie(name, path='/', domain=None)
+        # 删除名为name，并同时匹配domain和path的cookie。
+
+        # clear_all_cookies(path='/', domain=None)
+        # 删除同时匹配domain和path的所有cookie。
+
+        #clear_all_cookies()
+        #删除所有cookie
+
+        #执行清除cookie操作后，并不是立即删除了浏览器中的cookie，
+        # 而是给cookie值置空，并改变其有效期使其失效。真正的删除cookie是由浏览器去清理的。
+        
+        self.write("OK")
+
+if __name__=='__main__':
+    # 注意路由前面的/ 不能省略
+    app=tornado.web.Application(
+            [
+                (r"/",IndexHandler)
+            ]
+            #tornado提供了cookie的简单签名加密算法用于防止cookie被修改
+            #加密算法：
+            #import base64, uuid
+            #base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
+            #加密cookie的获取和设置
+            
+            # 设置一个带签名和时间戳的cookie，防止cookie被伪造
+            #set_secure_cookie(name, value, expires_days=30)
+            
+            #获取签名的cookie
+            #get_secure_cookie(name, value=None, max_age_days=31)
+            # 如果cookie存在且验证通过，返回cookie的值，否则返回None。max_age_day不同于expires_days，
+            # expires_days是设置浏览器中cookie的有效期，而max_age_day是过滤安全cookie的时间戳。
+            ,cookie_secret = "2hcicVu+TqShDpfsjMWQLZ0Mkq5NPEWSk9fi0zsSt3A="
+            ,debug=True
+        )
+    app.listen(9000)
+    tornado.ioloop.IOLoop.current().start()
---
+++ w3cWeb/tornado/studyDemo/template/01_static_file.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   01_static_file.py
+@Time    :   2019/07/01 15:35:43
+@Author  :   王翔 
+@Version :   1.0
+@Contact :   muumuu123@126.com
+@License :   (C)Copyright 2018-2019, 风蓝科技
+@Desc    :   None
+'''
+
+# here put the import lib
+
+import tornado.web
+import tornado.ioloop
+import tornado.httpserver
+import tornado.options
+import json
+import os
+
+from tornado.web import RequestHandler,StaticFileHandler
+
+# 定义服务器监听端口选项
+#限定默认值，数据类型
+tornado.options.define("port",default=9000,type=int,help="run server on the given port.")
+
+
+class IndexHandler(RequestHandler):
+    def get(self):
+        self.render('index.html',price1=100,price2=200,fileName="abcde")
+
+if __name__ == "__main__":
+    current_path = os.path.dirname(__file__)
+   
+    app=tornado.web.Application(
+            [
+                (r"/",IndexHandler)
+                # 提取url地址中127.0.0.1:9000/ 后面的文件映射到 path 路径 下对应的文件，默认是default_filename 定义的文件              
+                #注意在os.path.join时只需要输入文件夹名称即可否则会导致报错             
+                #r'^/()$' 这样定义正则表达更准确
+                #静态资源路由，通常应当放到路由的最后一项，防止拦截到前面的路由
+                # api路由通常应当设计成  r'/api/getId'这样的形式
+                ,(r'/(.*)',StaticFileHandler,{"path":os.path.join(current_path, "html"),"default_filename":"index.html"})
+            ]
+            ,debug=True
+            ,reload=False
+            # sys.path 指的是PYTHO解析器路径clrea
+            #os.path值的是操作系统的路径
+
+            # 通过本参数配置系统静态文件的路径
+            #定义后通过http://127.0.0.1:9000/static/html/index.html路径进行访问
+            #注意/static 与当前文件所在的文件夹static并没有任何关联关系，static是tornado的默认命名访问路径
+            # 为了配合nginx 的部署静态文件夹所在文件最好命名成static
+            ,static_path=os.path.join(current_path, "static")
+
+            #定义模板文件路径
+            ,template_path=os.path.join(current_path, "template")
+        )
+  
+    # print('join path:',os.path.dirname(__file__).join("statics"))
+    http_server=tornado.httpserver.HTTPServer(app)
+    http_server.listen(tornado.options.options.port)
+    tornado.ioloop.IOLoop().current().start()
+
+
